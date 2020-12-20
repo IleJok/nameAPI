bbdb9f60f5f289992db0068862f87b78b1cb3aee
IleJok <ilkka.jokela@gmail.com>
2020-12-20 14:17:16 +0200

    first commit to Solita's dev academy

---
+++ nameAPI/Controllers/NameController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace nameAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class NameController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<NameController> _logger;
+
+        public NameController(ILogger<NameController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<Name> Get()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new Name
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+    }
+}
---
+++ nameAPI/Name.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace nameAPI
+{
+    public class Name
+    {
+
+        public string name { get; set; }
+
+        public int amount { get; set; }
+    }
+}
